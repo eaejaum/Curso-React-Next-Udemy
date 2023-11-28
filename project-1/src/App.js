c76bc0d80179bd7864226fe2396f27e1d29b4637
eaejaum <jpedroalmeida179@gmail.com>
2023-11-28 10:23:39 -0300

    Data fetching

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -3,51 +3,14 @@ import React from 'react';
 
 class App extends React.Component {
   state = {
-      counter: 0,
-      posts: [
-        {
-          id: 1,
-          title: 'O título 1',
-          body: 'O corpo 1'
-        },
-        {
-          id: 2,
-          title: 'O título 2',
-          body: 'O corpo 2'
-        },
-        {
-          id: 3,
-          title: 'O título 3',
-          body: 'O corpo 3'
-        } 
-      ]
+      posts: []
   };
-  timeoutUpdate = null;
 
   componentDidMount() {
-    this.handleTimeout();
-  }
-
-  componentDidUpdate() {
-    this.handleTimeout();
-  }
-
-  componentWillUnmount() {
-    clearTimeout(this.timeoutUpdate);   
-  }
-
-  handleTimeout = () => {
-    const { posts, counter } = this.state;
-    posts[0].title = 'O título mudou';
-
-    this.timeoutUpdate = setTimeout(() => {
-      this.setState({ posts, counter: counter + 1 });
-    }, 1000)
   }
 
   render() {
     const { posts, counter } = this.state;
-    
 
     return ( 
       <div className="App">
