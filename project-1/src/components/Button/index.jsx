fcf0a8e39a395be978097678bb3e5a5828453fa1
eaejaum <jpedroalmeida179@gmail.com>
2023-12-04 12:23:52 -0300

    Organização de containers

---
+++ project-1/src/components/Button/index.jsx
@@ -0,0 +1,15 @@
+import './styles.css'
+
+import { Component } from "react";
+
+export class Button extends Component {
+    
+    render() {
+        const { text, onClick } = this.props;
+        return(
+        <button disabled className='button' onClick={onClick}>
+            {text}
+        </button>
+        )
+    }
+}
